dc3b10388bd71bb2710c4ead73cdcfd53bd2712f
Harish Kumar G <93115324+Haribose9912@users.noreply.github.com>
2023-09-02 22:53:56 +0530

    update run.bat
    update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 pip install webdriver-auto-update
 pytest -v -s --alluredir='./Reports' Testcases/Formfilltest.py --browser headless
 pytest -v -s --alluredir='./Reports' Testcases/Formfilltest.py --browser chrome
-allure serve "./Reports"
+
